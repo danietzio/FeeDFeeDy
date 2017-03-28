ef79b9f023e698e2acddacc8c2a7c7af4d395655
danietzio <dani.nasiri@gmail.com>
2017-03-28 13:56:46 +0430

    Split right Panel into two seprate components(2)

--- src/components/right-panel.js
+++ src/components/right-panel.js
@@ -7,28 +7,8 @@ export default class RightPanel extends React.Component {
 		return(
       <div id="right-panel-container">
 					<div className="fluid-container">
-						<div className="row" id="right-setting-panel">
-								<div className="col-xs-3 col-sm-3 col-md-3 middleDiv">
-											<img className="icons" src={ this.props.src + "/refresh.png" } />
-											<img className="icons" src={ this.props.src + "/down-arrow-white.png" } />
-								</div>
-								<div className="col-xs-5 col-sm-7 col-md-7 middleDiv">
-									<div id="bk_search">
-											<i className="fa fa-2x fa-search" aria-hidden="true"></i>
-											<input placeholder="Search Here..." type="text"></input>
-									</div>
-								</div>
-								<div className="col-xs-4 col-sm-2 col-md-2 middleDiv">
-									<div id="bk_profile">
-											<span>Dani Nasiri</span>
-											<a href="#"><div></div></a>
-									</div>
-								</div>
-						</div>
-
-						<div className="row" id="right-main-panel">
-
-						</div>
+						<RightTopMenu src="http://localhost:8000/assets/images"></RightTopMenu>
+						<RightConent></RightConent>
 					</div>
 			</div>
 		);
